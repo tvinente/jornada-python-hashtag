3e134769f97ffe8bf4dfb501f47cd43f1066cdb3
talyt <talyta_vinente@hotmail.com>
2024-10-14 18:58:00 -0300

    'Botão sair' adicionado ao site / Envio de mensagem de saída / limpreza de inteface

--- python_dev_hashzap/hashzap.py
+++ python_dev_hashzap/hashzap.py
@@ -13,11 +13,14 @@ def main(pagina):
 
             # Adicionar mensagem no chat
             chat.controls.append(ft.Text (f'{usuario_mensagem} : {texto_mensagem}'))
-        else:
+        elif tipo == 'entrada':
             usuario_mensagem = mensagem['usuario']
             chat.controls.append(ft.Text (f'{usuario_mensagem} entrou no chat', size=12, italic=True, color= ft.colors.ORANGE_500))
+        elif tipo == 'saida':
+            usuario_mensagem = mensagem['usuario']
+            chat.controls.append(ft.Text(f'{usuario_mensagem} saiu do chat', size=12, italic=True, color=ft.colors.RED_500))
         pagina.update()
-
+        
     pagina.pubsub.subscribe(enviar_mensagem_tunel)
 
     def enviar_mensagem(evento):
@@ -34,6 +37,19 @@ def main(pagina):
     campo_mensagem = ft.TextField (label= 'Digite uma mesagem', on_submit= enviar_mensagem)
     botao_enviar_mensagem = ft.ElevatedButton ('Enviar', on_click= enviar_mensagem)
 
+    def sair_chat(evento):
+        # Enviar mensagem de saída
+        pagina.pubsub.send_all({'usuario': nome_usuario.value, 'tipo': 'saida'})
+
+        #Limpar a interdace e voltar ao estado inicial
+        pagina.controls.clear()
+        pagina.add(texto)
+        pagina.add(nome_usuario)
+        pagina.add(botao_iniciar)
+        pagina.update()
+
+    botao_sair = ft.ElevatedButton('Sair', on_click=sair_chat)
+
     def entrar_popup(evento):
         pagina.pubsub.send_all ({'usuario': nome_usuario.value, 'tipo': 'entrada'})
 
@@ -43,15 +59,14 @@ def main(pagina):
         # Fechar o popup
         popup.open = False
 
-        # Remover o botão iniciar chat
+        #  Remover o botão iniciar chat e texto inicial
         pagina.remove(botao_iniciar)
         pagina.remove(texto)
 
-        # Criar o campo de mensagem do usuario
-        # Criar o botao de enviar mensagem do usuario
+        # Adicionar o campo de mensagem, botão de enviar e botão de sair
         pagina.add(ft.Row ([campo_mensagem, botao_enviar_mensagem]))
+        pagina.add(botao_sair)
         pagina.update()
-
 
     popup = ft.AlertDialog(
         open= False,
@@ -61,13 +76,16 @@ def main(pagina):
     )
 
     def entrar_chat(evento):
+        # Usando overlay em vez de pagina.dialog
         pagina.overlay.append(popup)
         popup.open = True
         pagina. update ()
 
     botao_iniciar = ft.ElevatedButton ('Iniciar chat', on_click= entrar_chat)
 
+    # Adicionar os controles iniciais (campo para nome, texto inicial e botão iniciar)
     pagina.add(texto)
+    # Adicionando o campo para inserir o nome do usuário
     pagina.add(nome_usuario)
     pagina.add(botao_iniciar)
 
