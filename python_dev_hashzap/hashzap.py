8f0114d5ff0cf0e3ecda5ad8cab464ca28274d8a
talyt <talyta_vinente@hotmail.com>
2024-10-14 19:02:26 -0300

    Primeira alternativa de chat, finalizada

--- python_dev_hashzap/hashzap.py
+++ python_dev_hashzap/hashzap.py
@@ -1,3 +1,23 @@
+'''
+Script Hashzap
+
+* Botão de iniciar chat
+* Popup para entrar no chat
+* Ao entrar no chat: -> Aparece para todo mundo
+  * Mensagem de você entrou no chat
+  * Campo e botão de enviar mensagem
+* Cada mensagem enviada: -> Aparece para todo mundo
+  * Nome: Texto da Mensagem
+
+ Instalar:
+
+* pip install flet
+* pip install flask
+* pip install flask-socketio
+* pip install simple-websocket
+  
+'''
+
 import flet as ft
 
 def main(pagina):
